ee6435b86b1ab7610fb01e67c52b4f594367bf8e
cat1314e <68625703+cat1314e@users.noreply.github.com>
2021-01-06 17:14:47 +0800

    Revert "添加登录"
    This reverts commit d53e8ae2abf1760b28b84882cefbe751c1faedca.

--- src/api/api.js
+++ src/api/api.js
@@ -3,17 +3,21 @@ import axios from 'axios'
 const ajax = (method, url, params) => {
     if (method === 'post') {
         return (
-            axios.post('http://data-test.co-study.com.cn/api' + url, params)
+            axios.post('http://data-test.co-study.com.cn/api' + url, {
+                params
+            })
         )
-    } else if (method === 'get') {
+    } else if (method === 'post') {
         return (
-            axios.get('http://data-test.co-study.com.cn/api' + url, {params: params,})
+            axios.get('http://data-test.co-study.com.cn/api' + url, {
+                params
+            })
         )
     }
 }
 
 const apis = {
-    user_login: (params) => ajax('post', '/user/login', params),
+    user_login: (params) => ajax('post', '/user/sign_in', params),
 }
 
 export default apis
